ca04b1d753f76d1f4bd58971ffb3f34c3983de13
Akshat <codepreneur.in@gmail.com>
2025-08-08 15:19:00 +0530

    feat/addons

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,54 @@
 import express from "express"
 import jwt from "jsonwebtoken"
+import userModal from "../db";
+import JWT_SECRET from "../config"
+import zod from "zod";
 
 const router = express.Router();
 
-router.get("/users",(req,res)=>{
-    res.send("Users")
+const signupBody = zod.body({
+    username:zod.string().email(),
+    firstName:zod.string(),
+    lastName:zod.string(),
+    password:zod.string()
+})
+router.post("/user/signup", async (req,res)=>{
+
+    const {sucess} = signupBody.safeParse(req.body);
+    if(!sucess){
+        return res.status(411).json({
+            message:"Email already taken / Incorrect inputs"
+        })
+    }
+
+    const existingUser = await userModal.findOne({
+        username:req.body.username
+    })
+    if(existingUser){
+        return res.sendStatus(411).json({
+            message:"Email already taken/Incorrect Inputs"
+        })
+    }
+
+    const user = await userModal.create({
+        firstName:firstName,
+        lastName:lastname,
+        username:username,
+        password:password
+    })
+    const userId = user._id;
+
+    if(userId){
+        const token = jwt.sign({
+            userId
+        },JWT_SECRET);
+        res.json({
+            message:"User Created successfully",
+            token:token
+        })
+
+    }
+
 
 })
-
 export default router
