5506383e4642595bb0123692b779d4eec24c978a
Akshat <codepreneur.in@gmail.com>
2025-08-08 15:49:13 +0530

    added authmiddleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+import JWT_SECRET from "../config";
+import jwt from "jsonwebtoken"
+
+function authMiddleware(req,res,next){
+
+    const authHeader = req.headers.authorization;
+
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        return res.status(403).json({
+            message:"Wrong Token"
+        })
+    }
+    const token = authHeader.split(' ')[1];
+
+    try{
+        const decode = jwt.verify(token,JWT_SECRET);
+        
+        if(decode.userId){
+            req.userId = decode.userId;
+            next();
+        } else{
+            return res.status(403).json({})
+        }
+
+    } 
+    catch(err){
+        return res.status(403).json({})
+
+    }
+};
+
+module.exports={
+    authMiddleware
+}
