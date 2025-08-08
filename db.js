dc204342701ebdc92673e9d5d064ab9fbdfe3d93
Akshat <codepreneur.in@gmail.com>
2025-08-08 11:21:59 +0530

    updated schema

--- db.js
+++ db.js
@@ -6,12 +6,38 @@ const Schema = mongoose.Schema;
 const ObjectId = mongoose.ObjectId;
 
 const userSchema = new Schema({
-    name: String,
-    email: String,
-    amount: Number
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
 })
 
 const userModal = mongoose.modal("User", userSchema);
 
 
-export default userModal;
+module.exports = {
+    userModal
+}
