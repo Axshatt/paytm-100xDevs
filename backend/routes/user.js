e57df5128a0aebf3a6d9fef009c9b1895133a39c
Akshat <codepreneur.in@gmail.com>
2025-08-11 13:05:23 +0530

    added route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,44 +8,44 @@ import authMiddleware from "../middleware/authMiddleware"
 const router = express.Router();
 
 
-router.post("/user/signup", async (req,res)=>{
+router.post("/user/signup", async (req, res) => {
     const signupBody = zod.body({
-    username:zod.string().email(),
-    firstName:zod.string(),
-    lastName:zod.string(),
-    password:zod.string()
-})
-    const {sucess} = signupBody.safeParse(req.body);
-    if(!sucess){
+        username: zod.string().email(),
+        firstName: zod.string(),
+        lastName: zod.string(),
+        password: zod.string()
+    })
+    const { sucess } = signupBody.safeParse(req.body);
+    if (!sucess) {
         return res.status(411).json({
-            message:"Email already taken / Incorrect inputs"
+            message: "Email already taken / Incorrect inputs"
         })
     }
 
     const existingUser = await userModal.findOne({
-        username:req.body.username
+        username: req.body.username
     })
-    if(existingUser){
+    if (existingUser) {
         return res.sendStatus(411).json({
-            message:"Email already taken/Incorrect Inputs"
+            message: "Email already taken/Incorrect Inputs"
         })
     }
 
     const user = await userModal.create({
-        firstName:firstName,
-        lastName:lastname,
-        username:username,
-        password:password
+        firstName: firstName,
+        lastName: lastname,
+        username: username,
+        password: password
     })
     const userId = user._id;
 
-    if(userId){
+    if (userId) {
         const token = jwt.sign({
             userId
-        },JWT_SECRET);
+        }, JWT_SECRET);
         res.json({
-            message:"User Created successfully",
-            token:token
+            message: "User Created successfully",
+            token: token
         })
 
     }
@@ -53,9 +53,58 @@ router.post("/user/signup", async (req,res)=>{
 
 })
 
-router.put("/",authMiddleware,(req,res)=>{
-    const userBody=zod.body({
-        password:zod.string(),
+router.put("/", authMiddleware, async (req, res) => {
+    const userBody = zod.body({
+        password: zod.string().optional(),
+        firstName: zod.string().optional(),
+        lastName: zod.string().optional()
     })
+
+    const { success } = userBody.safeParse(req.body);
+
+    if (!success) {
+        return res.status(403).json({
+            msg: "Incorrect credentials"
+        })
+    }
+    await userModal.updateOne(req.body, {
+        id: req.userId
+    })
+    res.json({
+        msg: "Updated Successfully"
+    })
+})
+
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await userModal.find({
+        $or: [
+            {
+                firstName: {
+                    "$regex": filter
+
+                }
+            }
+            ,
+            {
+                lastName: {
+                    "$regex": filter
+
+                }
+            }
+        ]
+    })
+
+    res.json({
+        user:users.map(user=>({
+            username:user.username,
+            firstName:user.firstName,
+            lastname:user.lastName,
+            _id:user._id
+
+        }))
+    })
+
 })
 export default router
